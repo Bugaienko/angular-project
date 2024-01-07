b4fff1a13d27a0eb1495a15b27a26302d6fe6e97
Bugaienko <sergiibugaienko@gmail.com>
2024-01-07 19:52:11 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MyCalculatorComponent} from "./my-calculator/my-calculator.component";
+import {MyEmptyPage} from "./empty-route/my-empty-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'calculator',
+    pathMatch: "full"
+  },
+  {
+    path: 'calculator',
+    component: MyCalculatorComponent,
+  },
+  {
+    path: '**',
+    component: MyEmptyPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MMyCalculatorModule} from "./m-my-calculator/m-my-calculator.module";
+import { NavigationComponent } from './navigation/navigation.component';
+import { MyEmptyPage } from './empty-route/my-empty-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MyEmptyPage,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MMyCalculatorModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
